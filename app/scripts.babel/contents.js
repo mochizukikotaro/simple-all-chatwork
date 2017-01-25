68f321898f2d8acd2a5efda4687876bb5e81a75d
Kotaro Mochizuki <mochizuki@basicinc.jp>
2017-01-25 18:45:40 +0900

    Remove setTimeout

--- app/scripts.babel/contents.js
+++ app/scripts.babel/contents.js
@@ -1,14 +1,24 @@
 'use strict'
 window.onload = function(){
-  console.log('piyo');
-}
+  const div = document.getElementById('_toList')
+  var content  = document.createElement('div')
+  content.setAttribute('id', 'SimpleAllBtn')
+  var text     = document.createTextNode('Simpleにすべて選択')
+  content.appendChild(text)
+  div.appendChild(content)
 
-document.getElementById('_toListSelectAll').addEventListener('click', function(){
-  const replace_text = () => {
+
+  document.getElementById('SimpleAllBtn').addEventListener('click', () => {
+
     let chatwork_textbox=document.getElementById('_chatText');
-    let to_array=chatwork_textbox.value.match(/\[.+\]/g);
     chatwork_textbox.value='';
-    for(let i=0;i<to_array.length;i++)chatwork_textbox.value+=to_array[i];
-  }
-  setTimeout(replace_text, 300)
-})
+
+    const li = document.getElementsByTagName('li')
+    for (var i=0; i<li.length; i++) {
+      var number = li[i].getAttribute('data-cwui-lt-value')
+      if(number !== null){
+        chatwork_textbox.value+='[To:'+number+']';
+      }
+    }
+  })
+}
